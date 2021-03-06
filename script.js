5d6a5896f95b6335887d6b88a0b7ed8481a7f084
Pedro Fermin <pedrojoel705@gmail.com>
2021-03-06 17:26:58 -0300

    calculos de total

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+const inputPrincipal = document.querySelector("#principal");
+const inputYears = document.querySelector("#years");
+const inputRate = document.querySelector("#rate");
+const spanRange = document.querySelector("#resultRange");
+const spanCompute = document.querySelector("#resultCompute");
+
+function compute() {
+    const interest = Number(inputPrincipal.value) * Number(inputRate.value) * Number(inputYears.value) / 100
+    spanCompute.textContent = `If you deposit ${inputPrincipal.value},
+    at an interest rate of ${inputRate.value}
+    You will receive an amount of ${interest},
+    in the year ${(new Date()).getFullYear() + Number(inputYears.value)}`;
+
+
+
+
 }
-        
+
+inputRate.oninput = (event) => {
+    spanRange.innerHTML = inputRate.value
+    console.log(event.target.value)
+}
+
+
+
+
