2d2633060c39716792beeae6da8053e2edbeb30e
Pedro Fermin <pedrojoel705@gmail.com>
2021-03-06 22:45:37 +0100

    final adjustments

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ const inputYears = document.querySelector("#years");
 const inputRate = document.querySelector("#rate");
 const spanRange = document.querySelector("#resultRange");
 const spanCompute = document.querySelector("#resultCompute");
+const errorMessageElement = document.querySelector('.error-message');
 
 function compute() {
+    validateForm();
     const interest = Number(inputPrincipal.value) * Number(inputRate.value) * Number(inputYears.value) / 100
-    spanCompute.textContent = `If you deposit ${inputPrincipal.value},
-    at an interest rate of ${inputRate.value}
-    You will receive an amount of ${interest},
-    in the year ${(new Date()).getFullYear() + Number(inputYears.value)}`;
-
-
-
-
+    spanCompute.innerHTML = `<p align="justify">If you deposit <span class="highlighted">${inputPrincipal.value} </span>,
+    at an interest rate of <span class="highlighted">${inputRate.value}</span>
+    You will receive an amount of <span class="highlighted"> ${interest}</span>,
+    in the year <span class="highlighted"> ${(new Date()).getFullYear() + Number(inputYears.value)}</span></p>`;
+    reset();
 }
 
-inputRate.oninput = (event) => {
+function reset() {
+   inputPrincipal.value = ''; 
+   inputYears.value = ''; 
+   inputRate.value = '';
+   spanRange.innerHTML = '10.25';
+   inputPrincipal.focus();
+}
+
+function validateForm () {
+    if(inputPrincipal.value <= 0) {
+        errorMessageElement.style.display = 'block';
+    } else if (errorMessageElement.style.display === 'block') {
+        errorMessageElement.style.display = 'none';
+    }
+}
+
+function updateRangeSpan() {
     spanRange.innerHTML = inputRate.value
-    console.log(event.target.value)
 }
-
-
-
-
